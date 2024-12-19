2f08c28792f01273783c2fcf183f6be6cddc965e
Jsp3r <120098007+Jsp3r@users.noreply.github.com>
2024-12-19 14:00:39 +0800

    Create ResultofOperations.py

---
+++ ResultofOperations.py
@@ -0,0 +1,28 @@
+num1 = int (input("Enter the first number : "))
+num2 = int (input("Enter the second number : "))
+num3 = int (input("Enter the third number : "))
+num4 = int (input("Enter the fourth number : "))
+num5 = int (input("Enter the fifth number : "))
+
+
+sum = num1 + num2
+diff = sum - num3
+prod = diff * num4
+qou = prod / num5
+
+print(f"Sum is: {sum}")
+print(f"Difference is: {diff}")
+print(f"Porduct is: {prod}")
+print(f"Qoutient is: {int(qou)}")
+
+Output:
+Enter the first number : 5
+Enter the second number : 5
+Enter the third number : 5
+Enter the fourth number : 4
+Enter the fifth number : 4
+
+Sum is: 10
+Difference is: 5
+Porduct is: 20
+Qoutient is: 5
